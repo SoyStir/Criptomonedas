11f062b4c12dd04f4813a1be035dac2cd9f83724
SoyStir <robinton333@gmail.com>
2022-01-20 17:56:54 -0400

    Cotizaciones de Criptomonedas

---
+++ app.js
@@ -0,0 +1,14 @@
+let endpoint = 'https://api.binance.com/api/v3/ticker/price'
+fetch(endpoint)
+    .then(response => response.json())
+    .then(data => mostrarData(data))
+    .catch( e => console.log(e))
+
+
+const mostrarData = (data) =>{
+    let body = ''
+    for (let i=0; i < data.length; i++){
+        body += `<tr><td>${data[i].symbol}</td><td>${data[i].price}</td></tr>`
+    }
+    document.getElementById('data').innerHTML = body
+}
